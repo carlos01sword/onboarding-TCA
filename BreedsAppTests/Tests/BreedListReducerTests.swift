2260fb32c2af9a6d39b1db7ae7b8df3613cd2e43
Carlos Costa <ca.costa@swordhealth.com>
2025-11-25 13:18:02 +0000

    add basic unit tests for search functionality

--- BreedsAppTests/Tests/BreedListReducerTests.swift
+++ BreedsAppTests/Tests/BreedListReducerTests.swift
@@ -32,3 +32,36 @@ struct BreedListReducerTests {
         }
     }
 }
+
+@Suite("Breed List Feature - Search Functionality")
+@MainActor
+struct SearchFunctionalityTests {
+    @Test
+    func testSearchBreedNoMatch() async throws {
+        let store = TestStore(initialState: MockData.makeState()) { BreedListReducer() }
+
+        await store.send(.searchQueryChanged("CAT MEOW MEOW DOESNT EXIST")) {
+            $0.searchQuery = "CAT MEOW MEOW DOESNT EXIST"
+            $0.filteredBreeds = []
+        }
+    }
+    @Test
+    func testSearchBreedWithMatch() async throws {
+        let breed = MockData.breed1
+        let store = TestStore(
+            initialState: MockData.makeState(breeds: [breed])
+        ) { BreedListReducer() }
+
+        await store.send(.searchQueryChanged("Breed 1")) {
+            $0.searchQuery = "Breed 1"
+            $0.filteredBreeds = IdentifiedArray(
+                uniqueElements: [
+                    BreedCellReducer.State(
+                        breed: breed,
+                        favoriteBreeds: $0.$favoriteBreeds
+                    )
+                ]
+            )
+        }
+    }
+}
